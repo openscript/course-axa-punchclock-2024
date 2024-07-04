5b61bc7ef2631f783a3c2e38be3354012c7d1a3c
Robin Bühler <git+r@obin.ch>
2024-07-04 09:13:27 +0000

    chore: initalize spring boot

---
+++ src/test/java/ch/axa/its/punchclock/PunchclockApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.axa.its.punchclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
