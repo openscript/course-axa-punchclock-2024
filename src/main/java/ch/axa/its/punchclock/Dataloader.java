071988125c75971d48a9e60891c552231818ea1f
Robin Bühler <git+r@obin.ch>
2024-07-10 07:54:34 +0000

    feat: add seed

---
+++ src/main/java/ch/axa/its/punchclock/Dataloader.java
@@ -0,0 +1,49 @@
+package ch.axa.its.punchclock;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import ch.axa.its.punchclock.domain.Category;
+import ch.axa.its.punchclock.domain.Entry;
+import ch.axa.its.punchclock.domain.Tag;
+import ch.axa.its.punchclock.repositories.CategoryRepository;
+import ch.axa.its.punchclock.repositories.EntryRepository;
+import ch.axa.its.punchclock.repositories.TagRepository;
+
+@Component
+public class Dataloader implements ApplicationRunner {
+
+  @Autowired
+  private CategoryRepository categoryRepository;
+
+  @Autowired
+  private TagRepository tagRepository;
+
+  @Autowired
+  private EntryRepository entryRepository;
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    var category = new Category();
+    category.setName("Arbeit");
+    categoryRepository.save(category);
+
+    var tag = new Tag();
+    tag.setName("Projekt: Punchclock");
+    tagRepository.save(tag);
+
+    var entry = new Entry();
+    var tags = new HashSet<Tag>();
+    tags.add(tag);
+    entry.setTags(tags);
+    entry.setCategory(category);
+    entry.setCheckIn(LocalDateTime.now().minusHours(2));
+    entry.setCheckOut(LocalDateTime.now());
+    entryRepository.save(entry);
+  }
+}
--- src/main/java/ch/axa/its/punchclock/domain/Entry.java
+++ src/main/java/ch/axa/its/punchclock/domain/Entry.java
@@ -10,7 +10,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
